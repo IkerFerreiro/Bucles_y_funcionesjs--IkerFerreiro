9e5656e671f59f60e8fe9f8d0126a561bc132201
IkerFerreiro <ikerferreiroclase@gmail.com>
2025-02-24 02:22:20 +0100

    v0.2

---
+++ ejercicio2.js
@@ -0,0 +1,18 @@
+const movies = [
+    { title: 'The Matrix', releaseYear: 1999 },
+    { title: 'Star Wars: Episode IV – A New Hope', releaseYear: 1977 },
+    { title: 'Inception', releaseYear: 2010 },
+    { title: 'Jurassic Park', releaseYear: 1993 },
+    { title: 'The Shawshank Redemption', releaseYear: 1994 },
+    { title: 'Pulp Fiction', releaseYear: 1994 },
+    { title: 'Avatar', releaseYear: 2009 },
+    { title: 'The Dark Knight', releaseYear: 2008 },
+    { title: 'Fight Club', releaseYear: 1999 },
+    { title: 'Forrest Gump', releaseYear: 1994 }
+  ];
+  
+  const moviesAfter2000 = movies.filter(movie => movie.releaseYear > 2000);
+  const moviesBefore2000 = movies.filter(movie => movie.releaseYear < 2000);
+
+    console.log(moviesAfter2000);
+    console.log(moviesBefore2000);
